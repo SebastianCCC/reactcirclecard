2d39ef2b0f0dfd78cd497da7906cd980f36dbe22
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-12-17 21:12:53 +0100

    add visual's data

--- src/visual.ts
+++ src/visual.ts
@@ -32,7 +32,7 @@ import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions
 import IVisual = powerbi.extensibility.visual.IVisual
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import ReactCircleCard from './component'
+import { ReactCircleCard, initialState } from './component'
 
 import './../style/visual.less'
 
@@ -47,5 +47,19 @@ export class Visual implements IVisual {
     ReactDOM.render(this.reactRoot, this.target)
   }
 
-  public update(options: VisualUpdateOptions) {}
+  public update(options: VisualUpdateOptions) {
+    if (options.dataViews && options.dataViews[0]) {
+      const dataView: DataView = options.dataViews[0]
+
+      ReactCircleCard.update({
+        textLabel: dataView.metadata.columns[0].displayName,
+        textValue: dataView.single.value.toString(),
+      })
+    } else {
+      this.clear()
+    }
+  }
+  private clear() {
+    ReactCircleCard.update(initialState)
+  }
 }
